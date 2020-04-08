1e50eeeb58b5bfd5e2c9850325bf80d480d66076
Salvo Miosi <salvo.ilmiosi@gmail.com>
2020-04-08 17:05:57 +0200

    added binary resources support

--- include/resource_load.h
+++ include/resource_load.h
@@ -10,4 +10,11 @@ SDL_RWops *getResourceRW(const char *RES_ID);
 
 std::string loadStringFromResource(const char *RES_ID);
 
+#define BINARY_START(name) _binary_##name##_start
+#define BINARY_END(name) _binary_##name##_end
+#define BINARY_SIZE(name) (BINARY_END(name) - BINARY_START(name))
+
+#define BINARY_DECLARE(name) extern char BINARY_START(name)[]; extern char BINARY_END(name)[];
+#define BINARY_STRING(name) std::string(BINARY_START(name), BINARY_SIZE(name))
+
 #endif // __RESOURCES_H__
